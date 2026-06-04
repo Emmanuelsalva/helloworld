802bc7a9e46bbc3adf586d35825357ab8d116a0b
Emmanuelsalva <salvatoreemmanuel05@gmail.com>
2026-06-05 01:51:22 +0300

    Add files via upload

---
+++ helloworld/src/main/java/vu/helloworld/Helloworld.java
@@ -0,0 +1,16 @@
+package vu.helloworld; //package
+
+
+public class Helloworld { // Main program starts
+
+    public static void main(String[] args) { // <-statement
+        
+        System.out.println("Hello World!"); // <-statement
+        
+        System.out.println(5+5); // <-statement
+        
+        int sum =10+15;
+          System.out.println(sum); // <-statement
+        
+    }
+}
